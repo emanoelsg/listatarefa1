352d3e27264830e0fc8830b3c4233e317ee03c83
Airget-01 <dapouco3@gmail.com>
2025-08-19 14:11:43 -0300

    .

--- lib/app/features/auth/presentation/controller/auth_controller.dart
+++ lib/app/features/auth/presentation/controller/auth_controller.dart
@@ -1,5 +1,5 @@
 // app/features/auth/presentation/controller/auth_controller.dart
-import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
